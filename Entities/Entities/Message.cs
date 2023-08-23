6319f8a699f7196d80b5073a996b1183cf82e4e1
joaoc <joaoc@JOAOCARA>
2023-08-22 22:45:30 -0300

    Adicionar arquivos de projeto.

---
+++ Entities/Entities/Message.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    [Table("TB_MESSAGE")]
+    public class Message : Notify
+    {
+        [Column("MSG_ID")]
+        public int Id { get; set; }
+
+        [Column("MSG_TITLE")]
+        public string Title { get; set; }
+
+        [Column("MSG_ACTIVE")]
+        public bool Active { get; set; }
+
+        [Column("MSG_DATEREGISTER")]
+        public DateTime DateRegister { get; set; }
+
+        [Column("MSG_DATEUPDATE")]
+        public DateTime DateUpdate { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        [Column(Order = 1)]
+        public string UserId { get; set; }
+
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
---
+++ Entities/Entities/Notify.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entities
+{
+    public class Notify
+    {
+        [NotMapped]
+        public string NameProperty { get; set; }
+
+        [NotMapped]
+        public string Message { get; set; }
+
+        [NotMapped]
+        public List<Notify> Notifies { get; set; }
+
+        /// <summary>
+        /// Inicializa classe com uma lista de notificações
+        /// </summary>
+        public Notify()
+        {
+            Notifies = new List<Notify>();
+        }
+
+        /// <summary>
+        /// Valida propriedade string
+        /// </summary>
+        /// <param name="value">Valor da propriedade</param>
+        /// <param name="nameProperty">Nome da propriedade</param>
+        /// <returns>True ou false</returns>
+        public bool ValidPropertyString(string value, string nameProperty)
+        {
+            if(string.IsNullOrWhiteSpace(value) || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifies.Add(new Notify { NameProperty = nameProperty, Message = "O campo " + nameProperty + " é obrigatório." });
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Valida propriedade int
+        /// </summary>
+        /// <param name="value">Valor da propriedade</param>
+        /// <param name="nameProperty">Nome da propriedade</param>
+        /// <returns>True ou false</returns>
+        public bool ValidPropertyInt(int value, string nameProperty)
+        {
+            if (value < 1 || string.IsNullOrWhiteSpace(nameProperty))
+            {
+                Notifies.Add(new Notify { NameProperty = nameProperty, Message = "O campo " + nameProperty + " é obrigatório." });
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
