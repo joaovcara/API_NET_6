fb6405fceba25e372a653ac10e1327c038fd3942
Joao Cara <joao.cara@outlook.com.br>
2023-08-24 21:48:55 -0300

    Implementação WebAPI, Token, Configurações

---
+++ WebAPI/Models/MessageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Models
+{
+    public class MessageViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public bool Active { get; set; }
+
+        public DateTime DateRegister { get; set; }
+
+        public DateTime DateUpdate { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
